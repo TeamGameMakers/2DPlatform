ce2fe1179b5766b4bb57332fe03a8c4a46516111
Ashechol <ashechol@qq.com>
2022-07-06 18:02:27 +0800

    爬坡优化

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(GameInput))]
@@ -8,18 +7,18 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
     private Animator _anim;
-    private Collider2D _coll;
 
     [Header("Movement")]
     [SerializeField] private float speed = 2.8f;
-    [SerializeField] private float speedMultiplier = 0.5f;
-    private float _speed;
+    [SerializeField] private float airSpeedMultiplier = 0.5f;
+    private float _currentSpeed;
 
     [Header("Jump")]
     [SerializeField] private float jumpVelocity = 5;
     [SerializeField] private float jumpMultiplier = 1.0f;
     [SerializeField] private float fallMultiplier = 1.0f;
     private bool _jumping;
+    private bool _canJump;
 
     [Header("Ground Check")] 
     [SerializeField] private bool grounded;
@@ -30,13 +29,13 @@ public class PlayerController : MonoBehaviour
     [Header("Slope Climb")] 
     [SerializeField] private float maxAngle;
     [SerializeField] private float rayLength;
-    [SerializeField] private bool onSlope;
     [SerializeField] private PhysicsMaterial2D noneFriction;
     [SerializeField] private PhysicsMaterial2D fullFriction;
+    private float _slopeAngle;
+    private bool _onSlope;
+    private bool _canMoveOnSlope;
+    private Vector2 _slopeDirection;
 
-    public float gravityScale;
-    public Vector2 velocity;
-        
     // Animator Parameters Hash
     private static readonly int Move = Animator.StringToHash("move");
     private static readonly int Grounded = Animator.StringToHash("grounded");
@@ -46,7 +45,6 @@ public class PlayerController : MonoBehaviour
     {
         _input = GetComponent<GameInput>();
         _rb = GetComponent<Rigidbody2D>();
-        _coll = GetComponent<Collider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _anim = GetComponent<Animator>();
     }
@@ -54,19 +52,18 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _input.JumpStartEvent += OnJumpStart;
-        _speed = speed;
+        _currentSpeed = speed;
     }
 
     private void Update()
     {
         AnimationUpdate();
-        GroundCheck();
-        gravityScale = _rb.gravityScale;
-        velocity = _rb.velocity;
     }
 
     private void FixedUpdate()
     {
+        GroundCheck();
+        SlopeCheck();
         Movement();
     }
 
@@ -74,28 +71,23 @@ public class PlayerController : MonoBehaviour
     {
         var xInput = _input.moveInput;
         
+        // 转向
         if (xInput != 0.0f) _spriteRenderer.flipX = xInput < 0;
 
-        if (xInput * _rb.velocity.x <= 0) _speed = grounded? speed : speed * speedMultiplier;
-        
-        if (!grounded)
+        if (grounded && !_onSlope &&  !_jumping)
         {
-            _rb.gravityScale = _jumping? jumpMultiplier : fallMultiplier;
+            _rb.velocity = new Vector2(_currentSpeed * xInput, 0.0f);
         }
-        else
+        else if (grounded && _onSlope && _canMoveOnSlope && !_jumping)
         {
-            _rb.gravityScale = fallMultiplier;
-            _speed = speed;
+            xInput *= _currentSpeed;
+            _rb.velocity = _slopeDirection * -xInput;
         }
-
-        var dir = SlopeCheck();
-        _rb.sharedMaterial = xInput == 0 ? fullFriction : noneFriction;
-        if (dir != Vector2.zero && !_jumping && grounded)
+        else if (!grounded)
         {
-            _rb.velocity = new Vector2(-dir.x * xInput * _speed, -dir.y * xInput * speed);
+            xInput *= _currentSpeed;
+            _rb.velocity = new Vector2(xInput, _rb.velocity.y);
         }
-        else 
-            _rb.velocity = new Vector2(xInput * _speed, _rb.velocity.y);
 
     }
 
@@ -103,38 +95,42 @@ public class PlayerController : MonoBehaviour
     {
         grounded = Physics2D.OverlapCircle(checkPos.position, checkRadius, checkLayer.value);
 
-        if (_rb.velocity.y < 0) _jumping = false;
-    }
+        if (_rb.velocity.y < 0.0f) _jumping = false;
 
-    private Vector2 SlopeCheck()
+        if (grounded && !_jumping && _canMoveOnSlope)
+            _canJump = true;
+    }
+    
+    private void SlopeCheck()
     {
         var hit = Physics2D.Raycast(checkPos.position, Vector2.down, rayLength, checkLayer);
 
         // 斜坡法向量和垂直向量的夹角
-        var angle = Vector2.Angle(Vector2.up, hit.normal);
+        _slopeAngle = Vector2.Angle(Vector2.up, hit.normal);
+
+        _onSlope = _slopeAngle > 0.0f;
+        _canMoveOnSlope = _slopeAngle <= maxAngle;
+
+        if (_onSlope && _canMoveOnSlope && _input.moveInput == 0.0f)
+            _rb.sharedMaterial = fullFriction;
+        else if (!_canMoveOnSlope)
+            _rb.sharedMaterial = noneFriction;
+        else
+            _rb.sharedMaterial = null;
         
         // 斜坡法向量的正交向量
-        var dir = Vector2.Perpendicular(hit.normal).normalized;
+        _slopeDirection = Vector2.Perpendicular(hit.normal).normalized;
         
         Debug.DrawRay(hit.point, hit.normal, Color.green);
-        Debug.DrawRay(hit.point, -dir, Color.red);
-
-        if (angle <= maxAngle && angle > 0)
-        {
-            onSlope = true;
-            return dir;
-        }
-        
-        onSlope = false;
-
-        return Vector2.zero;
+        Debug.DrawRay(hit.point, _slopeDirection, Color.red);
     }
 
     private void OnJumpStart()
     {
-        if (!grounded) return;
+        if (!_canJump) return;
+        _canJump = false;
         _jumping = true;
-        _rb.velocity = new Vector2(_input.moveInput * _speed, jumpVelocity);
+        _rb.velocity = new Vector2(0.0f, jumpVelocity);
         _anim.SetTrigger(Jump);
     }
 
@@ -145,7 +141,7 @@ public class PlayerController : MonoBehaviour
     }
     
 #if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
+    private void OnDrawGizmos()
     {
         Gizmos.color = grounded? Color.green : Color.red;
         Gizmos.DrawWireSphere(checkPos.position, checkRadius);
